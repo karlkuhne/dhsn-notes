c3dc21142ab0220d46ac430baf542023c369333b
karlkuhne <karlkuhne@t-online.de>
2026-01-29 18:53:15 +0100

    added suppoirt for bin

--- SEM1/Scripts/werkzeug.c
+++ SEM1/Scripts/werkzeug.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 100
-#define FILEPATH "C:\\Users\\karlk\\Dokumente\\DHSN\\dhsn-notes\\SEM1\\Scripts\\Files\\werkzeug.txt"
+#define FILEPATH "C:\\Users\\karlk\\Dokumente\\DHSN\\dhsn-notes\\SEM1\\Scripts\\Files\\werkzeug.dat"
+#define TXT 0
 
 typedef struct {
     char *name;
@@ -12,28 +13,60 @@ typedef struct {
 
 ttool **readAllTools(FILE *datei, int *count) {
     ttool **allTools = NULL;
-    char buffer[MAX], delimiter[] = " ,;", *token;
     *count = 0;
-    
-    while (fgets(buffer, MAX, datei) != NULL) {
-        allTools = realloc(allTools, sizeof(ttool *) * (*count + 1));
-        allTools[*count] = malloc(sizeof(ttool));
 
-        token = strtok(buffer, delimiter);
-        allTools[*count]->name = malloc(strlen(token) + 1);
-        strcpy(allTools[*count]->name, token);
-        
-        token = strtok(NULL, delimiter);
-        allTools[*count]->marke = malloc(strlen(token) + 1);
-        strcpy(allTools[*count]->marke, token);
+    if (TXT) { // Textdatei
+        char buffer[MAX], delimiter[] = " ,;", *token;
+        while (fgets(buffer, MAX, datei) != NULL) {
+            allTools = realloc(allTools, sizeof(ttool *) * (*count + 1));
+            allTools[*count] = malloc(sizeof(ttool));
 
-        token = strtok(NULL, delimiter);
-        allTools[*count]->preis = malloc(sizeof(float));
-        *(allTools[*count]->preis) = atof(token);
+            token = strtok(buffer, delimiter);
+            allTools[*count]->name = malloc(strlen(token) + 1);
+            strcpy(allTools[*count]->name, token);
+            
+            token = strtok(NULL, delimiter);
+            allTools[*count]->marke = malloc(strlen(token) + 1);
+            strcpy(allTools[*count]->marke, token);
 
-        (*count)++;
+            token = strtok(NULL, delimiter);
+            allTools[*count]->preis = malloc(sizeof(float));
+            *(allTools[*count]->preis) = atof(token);
+
+            (*count)++;
+        }
+    } else { // Binärdatei
+        ttool *tool;
+        while (1) {
+            int nameLen = fgetc(datei);
+            if (nameLen == EOF || nameLen == 0) break;
+            char *name = (char*)malloc(nameLen + 1);
+            fread(name, 1, nameLen, datei);
+            name[nameLen] = '\0';
+            
+            int markeLen = fgetc(datei);
+            if (markeLen == EOF) {  free(name); break; }
+            char *marke = (char*)malloc(markeLen + 1);
+            fread(marke, 1, markeLen, datei);
+            marke[markeLen] = '\0';
+
+            int preisLen = fgetc(datei);
+            if (preisLen == EOF) { free(name); free(marke); break;}
+            char *preis = (char*)malloc(preisLen + 1);
+            fread(preis, 1, preisLen, datei);
+            preis[preisLen] = '\0';
+            
+            allTools = realloc(allTools, sizeof(ttool *) * (*count + 1));
+            allTools[*count] = malloc(sizeof(ttool));
+            allTools[*count]->name = name;
+            allTools[*count]->marke = marke;
+            allTools[*count]->preis = malloc(sizeof(float));
+            *(allTools[*count]->preis) = atof(preis);
+            
+            free(preis); // nicht mehr benötigt
+            (*count)++;
+        }
     }
-
     return allTools;
 }
 
@@ -59,9 +92,26 @@ void addTools(ttool ***allTools, int *count) {
         strcpy(newTool->marke, newMarke);
         newTool->preis = malloc(sizeof(float));
         *(newTool->preis) = atof(newPreis);
-        file = fopen(FILEPATH, "a");
-        fprintf(file, "%s %s %.2f\n", newTool->name, newTool->marke, *(newTool->preis));
-        fclose(file);
+        if (TXT) { // Textdatei
+            file = fopen(FILEPATH, "a");
+            fprintf(file, "%s %s %.2f\n", newTool->name, newTool->marke, *(newTool->preis));
+            fclose(file);
+        } else { // Binärdatei
+            file = fopen(FILEPATH, "ab");
+            int nameLen = strlen(newTool->name);
+            fputc(nameLen, file);
+            fwrite(newTool->name, 1, nameLen, file);
+            int markeLen = strlen(newTool->marke);
+            fputc(markeLen, file);
+            fwrite(newTool->marke, 1, markeLen, file);
+            char preisStr[20];
+            sprintf(preisStr, "%.2f", *(newTool->preis));
+            int preisLen = strlen(preisStr);
+            fputc(preisLen, file);
+            fwrite(preisStr, 1, preisLen, file);
+            fclose(file);
+        }
+        
         *allTools = realloc(*allTools, sizeof(ttool *) * (*count + 1));
         (*allTools)[*count] = newTool;
         (*count)++;
