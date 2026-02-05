416fe9fe9787a77351f6f19d92acf59b5b79139a
karlkuhne <karlkuhne@t-online.de>
2026-02-05 15:33:29 +0100

    finished products script

--- SEM1/Scripts/produktdaten_aufg7.c
+++ SEM1/Scripts/produktdaten_aufg7.c
@@ -8,15 +8,21 @@ typedef struct {
     char *category;
     float *price;
     int *times;
-} product;
+} Product;
 
-product **readAllProducts(FILE *file, int *count) {
-    product **allProducts = NULL;
+typedef struct {
+    char *name;
+    int *times;
+} Category;
+
+
+Product **readAllProducts(FILE *file, int *count) {
+    Product **allProducts = NULL;
     *count = 0;
     char buffer[MAX], delimiter[] = ";", *token;
     while(fgets(buffer, MAX, file) != NULL) {
-        allProducts = realloc(allProducts, sizeof(product *) * (*count + 1));
-        allProducts[*count] = malloc(sizeof(product));
+        allProducts = realloc(allProducts, sizeof(Product *) * (*count + 1));
+        allProducts[*count] = malloc(sizeof(Product));
         
         token = strtok(buffer, delimiter);
         allProducts[*count]->name = malloc(strlen(token) + 1);
@@ -39,39 +45,99 @@ product **readAllProducts(FILE *file, int *count) {
     return allProducts;
 }
 
-void freeProduct(product *product) {
-    free(product->name);
-    free(product->category);
-    free(product->price);
-    free(product->times);
+Category **getAllCategories(Product **allProducts, int *count, int *numCategories) {
+    Category **allCategories = NULL;
+    *numCategories = 0;
+
+    for(int i=0; i<*count; i++) {
+        char *currentCat = allProducts[i]->category;
+
+        int found = -1;
+        for (int j=0; j<*numCategories; j++) {
+            if (strcmp(allCategories[j]->name, currentCat) == 0) {
+                found = j;
+                break;
+            }
+        }
+
+        if (found != -1) {
+            (*(allCategories[found]->times))++;
+        } else {
+            allCategories = realloc(allCategories, sizeof(Category *) * (*numCategories + 1));
+            allCategories[*numCategories] = malloc(sizeof(Category));
+                
+            allCategories[*numCategories]->name = malloc(strlen(currentCat) + 1);
+            strcpy(allCategories[*numCategories]->name, currentCat);
+            
+            allCategories[*numCategories]->times = malloc(sizeof(int));
+            *(allCategories[*numCategories]->times) = 1;
+                
+            (*numCategories)++;
+        }
+    }
+    return allCategories;
 }
 
-void freeAllProducts(product **allProducts, int count) {
+void freeProduct(Product *Product) {
+    free(Product->name);
+    free(Product->category);
+    free(Product->price);
+    free(Product->times);
+}
+
+void freeAllProducts(Product **allProducts, int count) {
     for(int i=0; i<count; i++) freeProduct(allProducts[i]);
     free(allProducts);
 }
 
+void freeCategory(Category *Category) {
+    free(Category->name);
+    free(Category->times);
+}
+
+void freeAllCategories(Category **allCategories, int numcategories) {
+    for(int i=0; i<numcategories; i++) freeCategory(allCategories[i]);
+    free(allCategories);
+}
+
 int main() {
     FILE *file = fopen("C:\\Users\\karlk\\Dokumente\\DHSN\\dhsn-notes\\SEM1\\Scripts\\Files\\produkte.csv", "r");
-    int count;
+    int count, numIndividualProducts = 0, numcategories;
     float total=0, mostExpensivePrice=0;
     char mostExpensiveName[MAX];
-    product **allProducts = readAllProducts(file, &count);
-    puts("");
+    Product **allProducts = readAllProducts(file, &count);
+
+    // Produktliste
     puts("Folgende Produkte wurden eingelesen:");
     for(int i=0; i<count; i++) {
         printf("%s (%s) kostet $%.2f - %d mal auf Lager\n", allProducts[i]->name, allProducts[i]->category, *(allProducts[i]->price), *(allProducts[i]->times));
         total+=(*(allProducts[i]->price) * *(allProducts[i]->times));
+        numIndividualProducts += *(allProducts[i]->times);
         if (*(allProducts[i]->price) > mostExpensivePrice) {
             strcpy(mostExpensiveName, allProducts[i]->name);
             mostExpensivePrice = *(allProducts[i]->price);
         }
     }
+
+    Category **allCategories = getAllCategories(allProducts, &count, &numcategories);
+
+    // Kategorieliste
     puts("");
-    printf("Der Gesamtwert aller Produkte betraegt %.2f\n", total);
-    printf("Das teuerste Produkt ist %s (kostet $%.2f)", mostExpensiveName, mostExpensivePrice);
-    puts("");
+    puts("Folgende Kategorien wurden eingelesen:");
+    for(int i=0; i<numcategories; i++) {
+        printf("%s - %d Produkte\n", allCategories[i]->name, *(allCategories[i]->times));
+    }
+
+    fclose(file);
+
+    file = fopen("C:\\Users\\karlk\\Dokumente\\DHSN\\dhsn-notes\\SEM1\\Scripts\\Files\\statistik.txt", "w");
+    fprintf(file, "Der Gesamtwert aller Produkte betraegt %.2f\n", total);
+    fprintf(file, "Das teuerste Produkt ist %s (kostet $%.2f)\n", mostExpensiveName, mostExpensivePrice);
+    fprintf(file, "Der Durchschnittpreis aller %d Produkte betraegt %.2f", numIndividualProducts, total/numIndividualProducts);
+
     freeAllProducts(allProducts, count);
+    freeAllCategories(allCategories, numcategories);
+
     fclose(file);
     return 0;
 }
